2332e03b29c1fdd164a08502673d12604f2ee72d
Ashen Indeewara <a.indeewara911@gmail.com>
2025-04-10 10:13:34 +0530

    :zap:

--- src/main/java/lk/ijse/simplechatapp/Server.java
+++ src/main/java/lk/ijse/simplechatapp/Server.java
@@ -1,4 +1,70 @@
 package lk.ijse.simplechatapp;
 
-public class Server {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server extends Application {
+
+    @FXML
+    private TextArea incommingMSG;
+
+    @FXML
+    private TextField message;
+
+    ServerSocket serverSocket;
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+    String messagee="";
+
+    public void initialize(){
+        new Thread(()->{
+            try {
+                serverSocket = new ServerSocket(4000);
+                incommingMSG.appendText("Server started.\n");
+                socket = serverSocket.accept();
+                incommingMSG.appendText("Client connected\n");
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                while (!message.equals("exit")){
+                    messagee = dataInputStream.readUTF();
+                    incommingMSG.appendText("Client: " +messagee + "\n");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+
+    @FXML
+    void sendMSG(ActionEvent event) throws IOException {
+        dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        messagee = message.getText();
+        dataOutputStream.writeUTF(messagee);
+        dataOutputStream.flush();
+        incommingMSG.appendText("Server: " + messagee + "\n");
+        message.setText("");
+        if (messagee.equals("exit")) {
+            socket.close();
+            serverSocket.close();
+        }
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/simplechatapp/server.fxml"))));
+        stage.show();
+    }
 }
